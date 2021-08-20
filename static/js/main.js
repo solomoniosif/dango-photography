33f3099aa5f54fa6985acfab5ce17aaace9f048b
Iosif Solomon <solomoniosif@gmail.com>
2021-08-20 17:49:54 +0300

    modified post creation templaates & started a navbar change

--- static/js/main.js
+++ static/js/main.js
@@ -46,3 +46,17 @@ mainForm.addEventListener("click", function (event) {
         totalForms.setAttribute('value', `${formCount + 1}`);
     }
 });
+
+
+var lastScrollTop;
+navbar = document.getElementById('navbar');
+window.addEventListener('scroll',function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrollTop > lastScrollTop){
+        navbar.style.top='-80px';
+    }
+    else{
+        navbar.style.top='0';
+    }
+    lastScrollTop = scrollTop;
+});
