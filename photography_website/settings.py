080c14c0f759c55282432f3d2800f2b48aadd302
Iosif Solomon <solomoniosif@gmail.com>
2021-08-22 08:43:18 +0300

    commented some code only used in development in settings.py

--- photography_website/settings.py
+++ photography_website/settings.py
@@ -185,38 +185,35 @@ SITE_ID = 1
 COMMENTS_XTD_MAX_THREAD_LEVEL = 1  # default is 0
 COMMENTS_XTD_LIST_ORDER = ('-thread_id', 'order')  # default is ('thread_id', 'order')
 
+#
+# # DEBUG_TOOLBAR_CONFIG
+# INTERNAL_IPS = ('127.0.0.1',)
+#
+# def show_toolbar(request):
+# 	return True
+#
 # DEBUG_TOOLBAR_CONFIG = {
-# 	"SHOW_TOOLBAR_CALLBACK": lambda request: True,
+# 	'INTERCEPT_REDIRECTS': False,
+# 	"SHOW_TOOLBAR_CALLBACK": show_toolbar,
+# 	'INSERT_BEFORE': '</head>'
 # }
-INTERNAL_IPS = ('127.0.0.1',)
-
-
-def show_toolbar(request):
-	return True
-
-
-DEBUG_TOOLBAR_CONFIG = {
-	'INTERCEPT_REDIRECTS': False,
-	"SHOW_TOOLBAR_CALLBACK": show_toolbar,
-	'INSERT_BEFORE': '</head>'
-}
-
-DEBUG_TOOLBAR_PANELS = [
-	'debug_toolbar.panels.history.HistoryPanel',
-	'debug_toolbar.panels.versions.VersionsPanel',
-	'debug_toolbar.panels.timer.TimerPanel',
-	'debug_toolbar.panels.settings.SettingsPanel',
-	'debug_toolbar.panels.headers.HeadersPanel',
-	'debug_toolbar.panels.request.RequestPanel',
-	'debug_toolbar.panels.sql.SQLPanel',
-	'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-	'debug_toolbar.panels.templates.TemplatesPanel',
-	'debug_toolbar.panels.cache.CachePanel',
-	'debug_toolbar.panels.signals.SignalsPanel',
-	'debug_toolbar.panels.logging.LoggingPanel',
-	'debug_toolbar.panels.redirects.RedirectsPanel',
-	'debug_toolbar.panels.profiling.ProfilingPanel',
-]
+#
+# DEBUG_TOOLBAR_PANELS = [
+# 	'debug_toolbar.panels.history.HistoryPanel',
+# 	'debug_toolbar.panels.versions.VersionsPanel',
+# 	'debug_toolbar.panels.timer.TimerPanel',
+# 	'debug_toolbar.panels.settings.SettingsPanel',
+# 	'debug_toolbar.panels.headers.HeadersPanel',
+# 	'debug_toolbar.panels.request.RequestPanel',
+# 	'debug_toolbar.panels.sql.SQLPanel',
+# 	'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+# 	'debug_toolbar.panels.templates.TemplatesPanel',
+# 	'debug_toolbar.panels.cache.CachePanel',
+# 	'debug_toolbar.panels.signals.SignalsPanel',
+# 	'debug_toolbar.panels.logging.LoggingPanel',
+# 	'debug_toolbar.panels.redirects.RedirectsPanel',
+# 	'debug_toolbar.panels.profiling.ProfilingPanel',
+# ]
 
 # Whitenoise Configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
