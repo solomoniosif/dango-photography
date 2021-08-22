a4fc6d1d8809b239e17ab94d069ae239bbbc52ee
Iosif Solomon <solomoniosif@gmail.com>
2021-08-22 08:29:50 +0300

    more configurations for deployment

--- photography_website/settings.py
+++ photography_website/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9r+6dqh-#+1w9d1=ad)phh3i#l#o-ncq-4gek$wip1=u-z%ws-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-photography.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
